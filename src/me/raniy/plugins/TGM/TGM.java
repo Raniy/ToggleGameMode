6f0efdb2d75871b98b51e25f4acc90994a17e2e5
Christopher Lohman <christopher_lohman@yahoo.com>
2011-10-07 03:02:34 -0500

    v1.4 Optimizations, new notification, VerboseMode

--- src/me/raniy/plugins/TGM/TGM.java
+++ src/me/raniy/plugins/TGM/TGM.java
@@ -24,11 +24,11 @@ public class TGM extends JavaPlugin {
 	private Logger myLog=Logger.getLogger("Minecraft");
 	public PluginDescriptionFile myDesc = null;
 	public Configuration myConfig = null;
-	public java.util.Properties myProperties= null;
+	private boolean inVerbose = true;
 	
 	// Constants
-	private GameMode creativeMode = GameMode.CREATIVE;
-	private GameMode survivalMode = GameMode.SURVIVAL;
+	private final GameMode creativeMode = GameMode.CREATIVE;
+	private final GameMode survivalMode = GameMode.SURVIVAL;
 	
 	//permissions
 	private String toggleGameModePermission = "ToggleGameMode.TGM";
@@ -38,9 +38,11 @@ public class TGM extends JavaPlugin {
 	private String toggleGameModeAlias = "tgm";
 	
 	// Notifications.
-	private String switchTo = ChatColor.WHITE + "Switched to:";
+	private String notPermissioned = "I'm sorry Dave. I can't let you do that.";
+	private String switchTo = ChatColor.WHITE + "GameMode switched to:";
 	private String switchTo1 = ChatColor.GREEN + " Creative Mode" + ChatColor.WHITE;
 	private String switchTo0 = ChatColor.GREEN + " Survival Mode" + ChatColor.WHITE;
+	
 
 	@Override
 	public void onDisable() {
@@ -68,34 +70,62 @@ public class TGM extends JavaPlugin {
     	// Do our command, whatever it might actually be called
     	if (((command.getName().equalsIgnoreCase(toggleGameMode)) || (command.getName().equalsIgnoreCase(toggleGameModeAlias))) && (!(player == null)))
     	{
-    		if(player.hasPermission(toggleGameModePermission)){
-    			// Check current game mode
-    			// Assume survival
-    			GameMode theCurrentGameMode = null;
-    			int currentGameMode = 0;
-    			
-    			theCurrentGameMode = player.getGameMode();
-    			currentGameMode = theCurrentGameMode.getValue();
+    		if(player.hasPermission(toggleGameModePermission) || player.isOp()){
+    			String verboseModeString = "";
+    			   			
     			// Determine game mode and flip it.
-    			switch(currentGameMode){
+    			switch(player.getGameMode().getValue()){
     				case(0):
     					// Switch to 1
     					player.setGameMode(this.creativeMode);
+    					if (this.isInVerbose()){
+    						verboseModeString = player.getName() + ": " + this.switchTo + this.switchTo1 + this.getVerboseModeLocationString(player);
+    						this.doLog(verboseModeString);
+    						Player onlinePlayers[] = this.getServer().getOnlinePlayers();
+    						
+    						for(int cnt = 0; cnt < onlinePlayers.length; cnt++){
+    							if (onlinePlayers[cnt].isOp()){
+    								onlinePlayers[cnt].sendMessage(verboseModeString);
+    							}
+    						}
+    					}
     					// Notify player of switch to 1
     					player.sendMessage(switchTo + switchTo1);
     					break;
     				case(1):
     					// Switch to 0
     					player.setGameMode(this.survivalMode);
+    					if (this.isInVerbose()){
+    						verboseModeString = player.getName() + ": " + this.switchTo + this.switchTo0 + this.getVerboseModeLocationString(player);
+    						this.doLog(verboseModeString);
+    						Player onlinePlayers[] = this.getServer().getOnlinePlayers();
+    						
+    						for(int cnt = 0; cnt < onlinePlayers.length; cnt++){
+    							if (onlinePlayers[cnt].isOp()){
+    								onlinePlayers[cnt].sendMessage(verboseModeString);
+    							}
+    						}
+    					}
     					// Notify player of switch to 0
     					player.sendMessage(switchTo + switchTo0);
     				break;
     			}
     			// Tell bukkit we handled the command
+    			
     			return true;
     		} else {
     			// This is our command, but they cant use it. Naughty.
-    			return false;
+    			// If we have a error message configured then send it and tell bukkit we handled the command.
+    			if(this.notPermissioned != "" || this.isInVerbose()){
+        			// If in Verbose mode then tell the console it happened. 
+        			if (this.isInVerbose()){
+        				this.doLog(player.getName() + " failed to switch game modes." + this.getVerboseModeLocationString(player));
+        			}
+    				player.sendMessage(notPermissioned);
+    				//Tell bukkit we process the command.
+    				return true;
+    			}
+  			return false;
     		}
     	}
     	// Tell Bukkit we didnt process this command
@@ -111,7 +141,6 @@ public class TGM extends JavaPlugin {
 	private void getMyYMLs() {
     	this.myDesc = this.getDescription();
     	this.myConfig = this.getConfiguration();
-    	this.myProperties = new java.util.Properties();
     	// Load our startup variables, set defaults if no config was present
     	// Permission
     	this.toggleGameModePermission = this.myConfig.getString("TGM.PermissionNode",toggleGameModePermission);
@@ -119,12 +148,31 @@ public class TGM extends JavaPlugin {
     	this.switchTo = this.myConfig.getString("TGM.ToggledMessage.Base",this.switchTo);
     	this.switchTo0 = this.myConfig.getString("TGM.ToggledMessage.Survival",this.switchTo0);
     	this.switchTo1 = this.myConfig.getString("TGM.ToggledMessage.Creative",this.switchTo1);
+    	this.notPermissioned = this.myConfig.getString("TGM.NoPermissionsMessage", this.notPermissioned);
+    	
+    	// Verbose Mode
+    	this.setInVerbose(this.myConfig.getBoolean("TGM.VerboseMode", this.isInVerbose()));
+
     	// Command and Alias
-    	// Until I figure a way to add our commands dynamically to the plugin.yml this is gonna have to be disabled.
+    	// Until I figure a way to add our commands dynamically to the plugin.yml this is going to have to be disabled.
     	//this.toggleGameMode = this.myConfig.getString("TGM.Command",this.toggleGameMode);
     	//this.toggleGameModeAlias = this.myConfig.getString("TGM.CommandAlias",this.toggleGameModeAlias);
+    	
     	//Make sure the config file is created.
     	this.myConfig.save();
 	}
 
+	public boolean isInVerbose() {
+		return inVerbose;
+	}
+
+	public boolean setInVerbose(boolean inVerbose) {
+		this.inVerbose = inVerbose;
+		return inVerbose;
+	}
+	private String getVerboseModeLocationString(Player player){
+		String retval = "";
+		retval = " in world: " + player.getWorld().getName() + " at X: " + player.getLocation().getBlockX() + " Y: " + player.getLocation().getBlockY() + " Z: " + player.getLocation().getBlockZ() ;
+		return retval;
+	}
 }
